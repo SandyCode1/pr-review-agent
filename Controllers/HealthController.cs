6deb2f8bea3c1a92c5b0353c22617b36ce97e91b
sandeep.singh <sandeep.singh@brightzone.us>
2026-06-08 12:22:09 +0530

    add health endpoint

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PRReviewAgent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("PR Review Agent Running");
+        }
+    }
+}
